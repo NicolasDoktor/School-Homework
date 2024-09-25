8e1a5fc08f7b479d754af154d02b4e3d01d54b15
Nicolas Doktor <144801889+NicolasDoktor@users.noreply.github.com>
2024-09-25 17:21:07 +0200

    Delete zadej vysku.py

--- zadej vysku.py
+++
@@ -1,8 +0,0 @@
-height = int(input("Enter your height:\n"))
-
-if height < 160:
-    print("You cant go.")
-else:
-    print("You can go.")   
-
-    # zadáte svou výšku a uvidíte jestly můžete jít nebo ne  
