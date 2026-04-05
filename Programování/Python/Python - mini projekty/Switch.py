dbc35ff9c07208d764859ebf0ca0e8c798224c7b
NicolasDoktor <nicolasek2312@gmail.com>
2026-04-05 18:47:53 +0200

    34

--- Programování/Python/Python - mini projekty/Switch.py
+++ Programování/Python/Python - mini projekty/Switch.py
@@ -18,4 +18,15 @@ class PDU(ABC):
     def isValid(self) -> bool:
         pass
 
+class EthFrame(PDU):
+    "Konkrétní model Ethernetového rámce"
+    def __init__(self, dmac: str, smac: str, type: int, payload: str, fcs: int = None):
+        if not (self.isValidMac(dmac) and self.isValidMac(smac)):
+            raise ValueError("Neplatný formát MAC adresy.")
+            
+        super().__init__(payload)
+        self._dmac = dmac
+        self._smac = smac
+        self._type = type
+        self._fcs = fcs if fcs is not None else self.calculateFcs()
 
